0eff51a29479c475ea3b9ff678fefb625d4164f8
Bruno Gonçalves <brunobgm002@gmail.com>
2022-10-27 23:59:32 -0300

    feat: add reducers

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useReducer, useState } from "react";
 
 interface CreateCycleData {
     task: string;
@@ -29,12 +29,58 @@ interface CyclesContextProviderProps {
     children: ReactNode;
 }
 
+interface CyclesState {
+    cycles: Cycle[],
+    activeCycleId: string | null
+}
+
 export const CyclesContext = createContext({} as CyclesContextType);
 
 export function CyclesContextProvider({ children }: CyclesContextProviderProps) {
-    const [cycles, setCycles] = useState<Cycle[]>([]);
-    const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
+    const [cyclesState, dispatch] = useReducer((state: CyclesState, action: any) => {
+
+        switch (action.type) {
+            case 'ADD_NEW_CYCLE':
+                return {
+                    ...state,
+                    cycles: [...state.cycles, action.payload.newCycle],
+                    activeCycleId: action.payload.newCycle.id
+                }
+            case 'INTERRUPT_CURRENT_CYCLE':
+                return {
+                    ...state,
+                    cycles: state.cycles.map(cycle => {
+                        if (cycle.id == state.activeCycleId) {
+                            return { ...cycle, interruptedDate: new Date() }
+                        } else {
+                            return cycle
+                        }
+                    }),
+                    activeCycleId: null
+                }
+            case 'MARK_CURRENT_AS_FINISHED':
+                return {
+                    ...state,
+                    cycles: state.cycles.map(cycle => {
+                        if (cycle.id == state.activeCycleId) {
+                            return { ...cycle, finishDate: new Date() }
+                        } else {
+                            return cycle
+                        }
+                    }),
+                    activeCycleId: null
+                }
+            default:
+                return state;
+        }
+    }, {
+        cycles:[],
+        activeCycleId: null
+    });
+
     const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
+
+    const { cycles, activeCycleId } = cyclesState;
 
     const activeCycle = cycles.find((cycle => cycle.id == activeCycleId))
 
@@ -43,13 +89,12 @@ export function CyclesContextProvider({ children }: CyclesContextProviderProps) 
     }
 
     function markCurrentCycleAsFinished() {
-        setCycles(state => state.map(cycle => {
-            if (cycle.id == activeCycleId) {
-                return { ...cycle, finishedDate: new Date() }
-            } else {
-                return cycle
+        dispatch({
+            type: 'MARK_CURRENT_CYCLE_AS_FINISHED',
+            payload: {
+                activeCycleId,
             }
-        }))
+        })
     }
 
     function CreateNewCycle(data: CreateCycleData) {
@@ -61,24 +106,26 @@ export function CyclesContextProvider({ children }: CyclesContextProviderProps) 
           minutesAmount: data.minutesAmount,
           startDate: new Date(),
         }
+
+        dispatch({
+            type: 'ADD_NEW_CYCLE',
+            payload: {
+                newCycle,
+            }
+        })
     
-        setCycles((state) => [...state, newCycle]);
-        setActiveCycleId(id);
         setAmountSecondsPassed(0);
     
         // reset();
     }
     
     function InterruptCurrentCycle() {
-        setCycles(state => state.map(cycle => {
-            if (cycle.id == activeCycleId) {
-            return { ...cycle, interruptedDate: new Date() }
-            } else {
-            return cycle
+        dispatch({
+            type: 'INTERRUPT_CURRENT_CYCLE',
+            payload: {
+                activeCycleId,
             }
-        }))
-
-        setActiveCycleId(null);
+        })
     }
 
     return(
